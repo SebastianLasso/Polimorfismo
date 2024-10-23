687c199892a2e2d51556784478fc2bf8d6353b2b
SebastianLasso <sebastianlassosevilla19@gmail.com>
2024-10-22 22:23:54 -0500

    Update Ejercicio1.polimorfismo.py

--- Ejercicio1.polimorfismo.py
+++ Ejercicio1.polimorfismo.py
@@ -9,3 +9,22 @@ print(multiplicar("El resultado es:",1))
 print(multiplicar(a,b))
 print(multiplicar("Adios, fue un placer haverte ayudado",1))
 print(multiplicar("Bye,  ",5))
+
+#Utilice dos funciones la de multiplicar y la de sumar 
+
+def sumar(num1, num2):
+    if isinstance(num1, (int, float)) and isinstance(num2, (int, float)):
+        # Sumar números
+        return num1 + num2
+    elif isinstance(num1, list) and isinstance(num2, list):
+        # Concatenar listas
+        return num1 + num2
+    elif isinstance(num1, str) and isinstance(num2, str):
+        # Concatenar cadenas de texto
+        return num1 + num2
+    else:
+        raise TypeError("Los tipos de objetos no son compatibles para la suma")
+
+print(sumar(3, 5)) 
+print(sumar([1, 2], [3, 4])) 
+print(sumar("Hola ")) 
