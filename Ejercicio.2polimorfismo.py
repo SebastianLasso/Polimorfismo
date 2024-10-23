a17a1458b31024859f537d5e2b30b495d36c48b9
SebastianLasso <sebastianlassosevilla19@gmail.com>
2024-10-22 22:39:29 -0500

    Update Ejercicio.2polimorfismo.py

--- Ejercicio.2polimorfismo.py
+++ Ejercicio.2polimorfismo.py
@@ -1,22 +1,47 @@
-class Animal:
- def sonido(self):
-  return "El animal hace un momento"
- 
-class Loro(Animal):
- def sonido(self):
-  return "El loro, habla"
-
-class Leon(Animal):
-    def sonido(self):
-      return "El leon, ruge"
+# Clase base
+class Vehiculo:
+    def moverse(self):
+        raise NotImplementedError("Este método debe ser sobrescrito en las clases hijas")
     
-animales = [Loro(),Leon()]
-for animal in animales:
-  print(animal.sonido())
+    def detenerse(self):
+        raise NotImplementedError("Este método debe ser sobrescrito en las clases hijas")
 
-a = Leon()
-print(a.sonido())
+# Clase hija: Auto
+class Auto(Vehiculo):
+    def moverse(self):
+        return "El auto está conduciendo por la carretera."
+    
+    def detenerse(self):
+        return "El auto se ha detenido en el semáforo."
 
-b = Loro()
-print(b.sonido())
+# Clase hija: Bicicleta
+class Bicicleta(Vehiculo):
+    def moverse(self):
+        return "La bicicleta está pedaleando por el carril bici."
+    
+    def detenerse(self):
+        return "La bicicleta se ha detenido al lado de la acera."
 
+# Clase hija: Barco
+class Barco(Vehiculo):
+    def moverse(self):
+        return "El barco está navegando por el océano."
+    
+    def detenerse(self):
+        return "El barco ha anclado en el puerto."
+
+# Clase hija: Avión
+class Avion(Vehiculo):
+    def moverse(self):
+        return "El avión está volando por el cielo."
+    
+    def detenerse(self):
+        return "El avión ha aterrizado en la pista."
+
+# Ejemplo de uso
+vehiculos = [Auto(), Bicicleta(), Barco(), Avion()]
+
+for vehiculo in vehiculos:
+    print(vehiculo.moverse())
+    print(vehiculo.detenerse())
+    print()
